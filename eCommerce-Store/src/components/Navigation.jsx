99e6ca8d389030daaafa79ebc9d9c34253954af3
paulbkim1 <paulbkim1@gmail.com>
2024-04-27 11:29:38 -0700

    update

--- eCommerce-Store/src/components/Navigation.jsx
+++ eCommerce-Store/src/components/Navigation.jsx
@@ -3,9 +3,11 @@ import { useState } from "react";
 import { MdMenu, MdShoppingCart } from "react-icons/md";
 import NavigationDrawer from "./NavigationDrawer";
 import CartDrawer from "./CartDrawer";
+import { useSelector } from "react-redux";
 
 export default function LandingNavigation() {
   const [activeDrawer, setActiveDrawer] = useState(null);
+  const totalQuantity = useSelector((state) => state.cart.totalQuantity);
 
   const toggleDrawer = (drawerType) => {
     if (activeDrawer === drawerType) {
@@ -15,23 +17,24 @@ export default function LandingNavigation() {
     }
   };
 
+  console.log(totalQuantity);
+
   return (
     <>
       <header className="flex items-center justify-center h-[8vh] border-b border-gray-400 fixed top-0 left-0 right-0 bg-white w-auto z-10">
-        <nav className="max-w-[500px] sm:max-w-[900px] px-[10px] sm:px-[20px] flex justify-between items-center w-full">
+        <nav className="max-w-[500px] sm:max-w-[900px] px-[16px] sm:px-[20px] flex justify-between items-center w-full">
           <NavLink
             to=""
             className="text-2xl font-bold hidden sm:block hover:scale-110 transition-all duration-300"
           >
             HappyHaul
           </NavLink>
-          <button
+          <MdMenu
+            className="text-3xl hover:text-4xl transition-all duration-300 sm:hidden cursor-pointer"
             onClick={() => toggleDrawer("navigation")}
-            className="sm:hidden"
-          >
-            <MdMenu className="text-3xl hover:scale-110 transition-all duration-300" />
-          </button>
-          <ul className="flex gap-4 sm:gap-6 items-center">
+          />
+
+          <ul className="flex gap-4 sm:gap-10 items-center sm:w-60 justify-between mr-5">
             <li>
               <NavLink
                 to="products/1"
@@ -48,10 +51,16 @@ export default function LandingNavigation() {
                 Categories
               </NavLink>
             </li>
-            <li className="flex items-center">
-              <button onClick={() => toggleDrawer("cart")}>
-                <MdShoppingCart className="text-2xl hover:scale-110" />
-              </button>
+            <li className="relative">
+              <MdShoppingCart
+                className="text-2xl hover:text-3xl transition-all duration-300 cursor-pointer"
+                onClick={() => toggleDrawer("cart")}
+              />
+              {totalQuantity > 0 && (
+                <span className="absolute -top-2 -right-4 bg-red-500 text-white text-xs rounded-full px-2 py-1">
+                  {totalQuantity}
+                </span>
+              )}
             </li>
           </ul>
         </nav>
